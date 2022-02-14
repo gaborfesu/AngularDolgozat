9e9cfb4136eff0d75532da77792b0407478275da
Sallai András <andrewteki@gmail.com>
2022-02-14 21:24:22 +0100

    Kezdés

---
+++ app/Http/Controllers/VehiclesController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVehiclesRequest;
+use App\Http\Requests\UpdateVehiclesRequest;
+use App\Models\Vehicles;
+
+class VehiclesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Vehicles::all();        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreVehiclesRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreVehiclesRequest $request)
+    {
+        return Vehicles::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vehicles  $vehicles
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vehicles $vehicles)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Vehicles  $vehicles
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vehicles $vehicles)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateVehiclesRequest  $request
+     * @param  \App\Models\Vehicles  $vehicles
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateVehiclesRequest $request, Vehicles $vehicles)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vehicles  $vehicles
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vehicles $vehicles)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_02_14_190848_create_vehicles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('plate', 20);
+            $table->string('brand', 20);
+            $table->integer('year');
+            $table->decimal('price', 10, 2);
+            $table->boolean('sold');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
